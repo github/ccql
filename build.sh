e8726410a504c51d877ac44f74c63b024b313fb5
Shlomi Noach <shlomi-noach@github.com>
2016-01-11 14:04:33 +0100

    golang 1.5 vendor support and usage; included dependant packages

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ timestamp=$(date "+%Y%m%d%H%M%S")
 gobuild="go build -o $buildpath/$target go/cmd/ccmysql/main.go"
 
 echo "Building linux binary"
-echo "GOOS=linux GOARCH=amd64 $gobuild" | bash
+echo "GO15VENDOREXPERIMENT=1 GOOS=linux GOARCH=amd64 $gobuild" | bash
 (cd $buildpath && tar cfz ./ccmysql-binary-linux-${timestamp}.tar.gz $target)
 
 echo "Building OS/X binary"
-echo "GOOS=darwin GOARCH=amd64 $gobuild" | bash
+echo "GO15VENDOREXPERIMENT=1 GOOS=darwin GOARCH=amd64 $gobuild" | bash
 (cd $buildpath && tar cfz ./ccmysql-binary-osx-${timestamp}.tar.gz $target)
 
 echo "Binaries are:"
