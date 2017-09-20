2e4edf78b3e695c106cf591bcfb511a93d267c22
AnshumanTripathi <anshuman.tripathi@clover.com>
2017-09-20 15:27:12 -0700

    Updated formatting and QuerySchema with WaitGroup

--- go/cmd/ccql/main.go
+++ go/cmd/ccql/main.go
@@ -41,7 +41,7 @@ func main() {
 	password := flag.String("p", "", "MySQL password")
 	askPassword := flag.Bool("ask-pass", false, "prompt for MySQL password")
 	credentialsFile := flag.String("C", "", "Credentials file, expecting [client] scope, with 'user', 'password' fields. Overrides -u and -p")
-	schemaList := flag.String("s", "information_schema", "List of Schema to query from.")
+	databases := flag.String("d", "information_schema", "List of databases to query from.")
 	hostsList := flag.String("h", "", "Comma or space delimited list of hosts in hostname[:port] format. If not given, hosts read from stdin")
 	hostsFile := flag.String("H", "", "Hosts file, hostname[:port] comma or space or newline delimited format. If not given, hosts read from stdin")
 	queriesText := flag.String("q", "", "Query/queries to execute")
@@ -96,9 +96,9 @@ func main() {
 	if *credentialsFile != "" {
 		mySQLConfig := struct {
 			Client struct {
-				       User     string
-				       Password string
-			       }
+				User     string
+				Password string
+			}
 		}{}
 		gcfg.RelaxedParserMode = true
 		err := gcfg.ReadFileInto(&mySQLConfig, *credentialsFile)
@@ -119,7 +119,7 @@ func main() {
 		*password = string(passwd)
 	}
 
-	schemas := strings.Split(*schemaList, ",")
+	schemas := strings.Split(*databases, ",")
 
 	if err := logic.QuerySchemas(hosts, *user, *password, schemas, queries, *maxConcurrency, *timeout); err != nil {
 		os.Exit(1)
--- go/logic/ccql.go
+++ go/logic/ccql.go
@@ -6,13 +6,14 @@ import (
 	"strings"
 
 	"github.com/outbrain/golib/sqlutils"
+	"sync"
 )
 
 // queryHost connects to a given host, issues the given set of queries, and outputs the results
 // line per row in tab delimited format
-func queryHost(host string, user string, password string, defaultSchema string, queries []string, timeout float64) error {
-	log.Println("Running for schema", defaultSchema)
-	mysqlURI := fmt.Sprintf("%s:%s@tcp(%s)/%s?timeout=%fs", user, password, host, defaultSchema, timeout)
+func queryHost(host string, user string, password string, schema string, queries []string, timeout float64) error {
+	mysqlURI := fmt.Sprintf("%s:%s@tcp(%s)/%s?timeout=%fs", user, password, host, schema, timeout)
+	fmt.Println(mysqlURI)
 	db, _, err := sqlutils.GetDB(mysqlURI)
 	if err != nil {
 		return err
@@ -24,74 +25,43 @@ func queryHost(host string, user string, password string, defaultSchema string, 
 			return err
 		}
 		for _, row := range resultData {
-			output := []string{host}
+			output := []string{host, schema}
 			for _, rowCell := range row {
 				output = append(output, rowCell.String)
 			}
 			rowOutput := strings.Join(output, "\t")
-			fmt.Println(defaultSchema, rowOutput)
+			fmt.Println(rowOutput)
 		}
 	}
 	return nil
 }
 
-// QueryHosts will issue concurrent queries on given list of hosts
-func QueryHosts(hosts []string, user string, password string, defaultSchema string, queries []string, maxConcurrency uint, timeout float64) (anyError error) {
-	concurrentHosts := make(chan bool, maxConcurrency)
-	completedHosts := make(chan bool)
-
-	for _, host := range hosts {
-		go func(host string) {
-			concurrentHosts <- true
-			if err := queryHost(host, user, password, defaultSchema, queries, timeout); err != nil {
-				anyError = err
-				log.Printf("%s %s", host, err.Error())
-			}
-			<-concurrentHosts
-
-			completedHosts <- true
-		}(host)
-	}
-	// Barrier. Wait for all to complete
-	for range hosts {
-		<-completedHosts
-	}
-	return anyError
-}
-
 func QuerySchemas(hosts []string, user string, password string, schemas []string, queries []string, maxConcurrency uint, timeout float64) (anyError error) {
 	concurrentHosts := make(chan bool, maxConcurrency)
 	completedHosts := make(chan bool)
-
-	concurrentSchemas := make(chan bool, maxConcurrency)
-	completedSchemas := make(chan bool)
+	var wg sync.WaitGroup
 
 	for _, host := range hosts {
 		go func(host string) {
+			wg.Add(len(schemas))
 			concurrentHosts <- true
-			//For each host, run all queries for the respective schema
+			// For each host, run all queries for the respective schema
 			for _, schema := range schemas {
 				go func(schema string) {
-					concurrentSchemas <- true
 					if err := queryHost(host, user, password, schema, queries, timeout); err != nil {
 						anyError = err
 						log.Printf("%s %s", host, err.Error())
 					}
-					<-concurrentSchemas
-					completedSchemas <- true
+					defer wg.Done()
 				}(schema)
 			}
-
-			// Barrier. Wait for all to complete
-			for range schemas {
-				<-completedSchemas
-			}
-
+			wg.Wait()
 			<-concurrentHosts
-
 			completedHosts <- true
 		}(host)
+
 	}
+
 	// Barrier. Wait for all to complete
 	for range hosts {
 		<-completedHosts
@@ -99,4 +69,3 @@ func QuerySchemas(hosts []string, user string, password string, schemas []string
 
 	return anyError
 }
-
