78031d6eaeae9b5f72c6427fc0fd1117f5d55482
Shlomi Noach <shlomi-noach@github.com>
2017-11-23 12:06:55 +0200

    Requiring go1.9

--- build.sh
+++ build.sh
@@ -13,5 +13,7 @@ echo "Building OS/X binary"
 echo "GO15VENDOREXPERIMENT=1 GOOS=darwin GOARCH=amd64 $gobuild" | bash
 (cd $buildpath && tar cfz ./ccql-binary-osx-${timestamp}.tar.gz $target)
 
+echo "Built with $(go version)"
+
 echo "Binaries found in:"
 ls -1 $buildpath/ccql-binary*${timestamp}.tar.gz
